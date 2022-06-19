bd2b7d7e53e8e00482b6c933a1c7fabe491eb85a
ImranRH321 <imranrh247@gmail.com>
2022-06-19 20:07:12 +0600

    ato je pera dilo data ta load hoito ja bolar bahaire

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
-const port = process.env.PORT || 5000
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
 
-const app = express()
-// middleware 
-app.use(cors())
-app.use(express.json)
+// middleware
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Ema John server is Running ')
-})
+app.get("/", (req, res) => {
+  res.send("Ema john server is Running ");
+});
+/* ------------------------------------ */
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e3ox5.mongodb.net/?retryWrites=true&w=majority`;
+console.log("uri", uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+/* ============================================================= */
+async function run() {
+  try {
+    await client.connect();
+    //  emaJohn1.product
+    const productCollection = client.db("emaJohn1").collection("product");
+    /* read */
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const result = await cursor.toArray();
+      console.log(result);
+      res.send(result);
+      /* ... */
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+/* ==================================================================== */
 app.listen(port, () => {
-    console.log('Ema Jhon server is Running port = ', port);
-})
+  console.log(`'Ema john server is Running Port = ${port}`);
+});
